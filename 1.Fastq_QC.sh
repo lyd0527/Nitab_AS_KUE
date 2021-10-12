daa13486d09387dc12d429b8ded307bd7f8ceea3
Yuanda Lv <lyd0527@users.noreply.github.com>
2021-10-12 19:15:38 +0800

    Update 1.Fastq_QC.sh

--- 1.Fastq_QC.sh
+++ 1.Fastq_QC.sh
@@ -1,5 +1,9 @@
-### Fastq cleaning
-Fastp
+### Cleaning
+nt=30
+sample_1=test_1.fq.gz
+sample_2=test_2.fq.gz
+
+fastp -q 20 -u 40 -M 0 -n 5 -l 50 -w $nt -i $sample_1 -I $sample_2 -o $sample_1.pe -O $sample_2.pe
 
 ### QC
-FastQC
+./FastQC/fastqc -t 50 *.fq.gz *.pe
