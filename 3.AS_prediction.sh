9382e8c689f26458960f1a9f961bf3fa3f2e12b0
Yuanda Lv <lyd0527@users.noreply.github.com>
2021-10-12 19:11:47 +0800

    Update 3.AS_prediction.sh

--- 3.AS_prediction.sh
+++ 3.AS_prediction.sh
@@ -6,8 +6,13 @@
 #############################################################
 #### AS prediction(SUPPA)
 #############################################################
-
+conda install -c bioconda suppa
+suppa.py generateEvents -i Nitab-K.gtf -o test.event -f ioe -e SE SS MX RI FL
 
 #############################################################
 #### AS prediction(ASprofile)
 #############################################################
+./ASprofile.b-1.0.4/extract-as Nitab-K.gtf nitab.hdrs >as_events.txt
+perl ASprofile.b-1.0.4/summarize_as.pl tobacco-K-Nitab-v4.5.gtf as_events.txt -p asp
+cut -f 2 asp.as.nr | sort | uniq -c
+
