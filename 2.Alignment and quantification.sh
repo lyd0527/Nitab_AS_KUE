5a8822ed1ff6561dc42a546db90d82824491d546
Yuanda Lv <lyd0527@users.noreply.github.com>
2021-10-12 19:18:00 +0800

    Update 2.Alignment and quantification.sh

--- 2.Alignment and quantification.sh
+++ 2.Alignment and quantification.sh
@@ -1,11 +1,10 @@
 #### Alignment
 
 id=test
-db_star=
+db_star=Nitab_star
 PE1=fastq2
 PE2=fastq1
 nt=30
-
 
 STAR --twopassMode Basic --outSAMstrandField intronMotif \
                --outFilterIntronMotifs RemoveNoncanonical --outSAMattrIHstart 0 \
@@ -18,14 +17,12 @@ STAR --twopassMode Basic --outSAMstrandField intronMotif \
 #### RABT
 #############################################################
 
-stringtie ....
-
+samtools merge $id1.bam $id2.bam $id3.bam ....> merged.bam
+stringtie merged.bam -G nitab.gtf -o Nitab-K.gtf -p 50
 
 #############################################################
 #### Quantification
 #############################################################
 
-featureCounts -T $nt -p -g gene_id -a $gtf -o Samples.featureCounts.txt *.bam
-
-
+featureCounts -T $nt -p -g gene_id -a Nitab-K.gtf -o Counts.txt *.bam
 
